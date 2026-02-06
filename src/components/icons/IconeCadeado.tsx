79f1ab04011c171fb37c925cf3e6c0168afa030f
Anaildo silva <anaildonascimentosilva074@gmail.com>
2026-02-06 03:32:02 -0300

    ✨ feat (frontend): adiciona ícones

---
+++ src/components/icons/IconeCadeado.tsx
@@ -0,0 +1,7 @@
+export function IconeCadeado() {
+  return (
+    <svg className="size-5 shrink-0 text-text" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 11V7a4 4 0 118 0m-4 8v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2z" />
+    </svg>
+  )
+}
