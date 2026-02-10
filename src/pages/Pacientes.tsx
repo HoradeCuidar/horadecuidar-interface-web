c43c641186e3541da6b1f15dc7613f57842f6a76
Anaildo silva <anaildonascimentosilva074@gmail.com>
2026-02-09 22:14:50 -0300

    ✨ feat (frontend): adiciona page gerenciamento de pacientes

---
+++ src/pages/Pacientes.tsx
@@ -0,0 +1,9 @@
+export function Pacientes() {
+  return (
+    <div className="p-8">
+      <h1 className="font-heading text-2xl font-semibold text-text">
+        Gerenciar Pacientes
+      </h1>
+    </div>
+  )
+}
