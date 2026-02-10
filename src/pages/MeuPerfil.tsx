fee7e00bf561bdd302fa18ea122930e56af3a864
Anaildo silva <anaildonascimentosilva074@gmail.com>
2026-02-09 22:16:40 -0300

    ✨ feat (frontend): adiciona page perfil usuários

--- src/pages/MeuPerfil.tsx
+++ src/pages/MeuPerfil.tsx
@@ -1,8 +1,8 @@
-export function Home() {
+export function MeuPerfil() {
   return (
     <div className="p-8">
       <h1 className="font-heading text-2xl font-semibold text-text">
-        Hdc
+        Meu perfil
       </h1>
     </div>
   )
