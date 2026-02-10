e3ec63c8cbd59a4a34f09766bcf9048fc55cd4a8
Anaildo silva <anaildonascimentosilva074@gmail.com>
2026-02-09 22:13:59 -0300

    ✨ feat (frontend): adiciona page gerenciamento de profissionais

---
+++ src/pages/Profissionais.tsx
@@ -0,0 +1,9 @@
+export function Profissionais() {
+  return (
+    <div className="p-8">
+      <h1 className="font-heading text-2xl font-semibold text-text">
+        Gerenciamento de Profissionais da Saúde
+      </h1>
+    </div>
+  )
+}
